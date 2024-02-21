24144883188cb5e633523e8fcdbde8d80e89b645
AJ Beckner <ajbeckner@gmail.com>
2024-02-21 16:54:03 -0500

    mountDesk

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import net from 'net'
 import { Cell, jam, Atom, cue } from '@urbit/nockjs'
+import { python } from 'pythonia'
 
 import { newtEncode, newtDecode, primitiveToAtom, atomToPrimitive } from './lib/bytes.js'
 import { nounToTree, unwrapUnit } from './lib/noun.js'
@@ -23,12 +24,29 @@ function Clack({ ship }) {
         responseParser: parseThreadResponse
       })
     },
+    mountDesk: async function (deskName) {
+      return await sendKahn({
+        ship,
+        noun: await Thread(`
+=/  m  (strand ,vase)
+;<  [=ship =desk =case]  bind:m  get-beak 
+;<  ~  bind:m  
+=/  pax=path  (en-beam [ship ${deskName} case] /)
+(poke-our %hood %kiln-mount !>([pax ${deskName}]))  
+(pure:m !>('success'))
+`),
+        responseParser: parseThreadResponse
+      })
+    },
     runThread: async function (thread, responseParser=parseThreadResponse) {
       return await sendKahn({
         ship,
         noun: thread,
         responseParser,
       })
+    },
+    close: async function(argument) {
+      python.exit()
     }
   }
 }
--- index.test.js
+++ index.test.js
@@ -6,3 +6,10 @@ test('createDesk', async function () {
   expect(noun).toBe('success');
 });
 
+test('mountDesk', async function () {
+	const clack = Clack({ ship: '../clack-scratchpad/zod' })
+	const noun = await clack.mountDesk('%js-created')
+  expect(noun).toBe('success');
+  await clack.close()
+});
+
